700808a0843ceb71f6fdae5dd46243c78e213b47
JuansebastianReyes <reyes_garcia1103@hotmail.com>
2021-05-28 23:23:40 -0500

    Add Sweetaler2

--- aplicativoTesis/src/app/components/login/login.component.ts
+++ aplicativoTesis/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -27,11 +27,13 @@ export class LoginComponent implements OnInit {
     const usuario = this.form.value.usuario;
     const password = this.form.value.password;
 
+    //Priemras pruebas para la redireccion del login
     if(usuario=="admin1" && password=="admin123"){
       this.router.navigate(['users/admin']);
     }else if (usuario=="leader1" && password=="leader123"){
       this.router.navigate(['users/leader']);
     }else if(usuario=="member1" && password=="member123"){
+      const url = 'users/member';
       this.router.navigate(['users/member']);
     }else{
       this.error();
@@ -39,8 +41,20 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  //Funcion para muestra error en el login
   error(){
-    alert("Usuario o Contraseña invalido");
+    Swal.fire({
+      icon: 'error',
+      title: 'Usuario o Contraseña inválido',
+      showConfirmButton: false,
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Inténtalo de Nuevo'
+    })
   }
 
+  succes(url:string){
+
+    this.router.navigate([url]);
+  }
 }
