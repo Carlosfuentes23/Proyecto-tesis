699a97eb2494f6277903859e1d483f4a034bef5e
Sebastian Reyes <reyes_garcia1103@hotmail.com>
2021-07-11 23:06:40 -0500

    Se completan vista de fases y se inicia la de proyecto

---
+++ aplicativoTesis/src/app/interfaces/project.interface.ts
@@ -0,0 +1,9 @@
+export interface Project{
+  name: string;
+  leader: string;
+  dateCreate: number;
+  dateEstimada: number;
+  description: string;
+  integrantes: string[];
+  reuniones: string[];
+}
