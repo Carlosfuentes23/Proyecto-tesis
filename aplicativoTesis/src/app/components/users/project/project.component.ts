318117814d1fec6efeb66643edbf83cd3d480773
Sebastian Reyes <reyes_garcia1103@hotmail.com>
2021-08-17 23:18:13 -0500

    Fin pagina proyecto

--- aplicativoTesis/src/app/components/users/project/project.component.ts
+++ aplicativoTesis/src/app/components/users/project/project.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Project } from 'src/app/interfaces/project.interface';
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
+
+  proyect : Project ={
+    name:"PeliApp",
+    leader:"Carlos Fuentes",
+    dateCreate: Date.now(),
+    dateEstimada: Date.now(),
+    description:"La mejor app de peliculas",
+    integrantes: ["Carlos Fuentes", "Sebastian Reyes", "Camilo Rozo"],
+    reuniones:["reunion 1","reunion 2", "reunion 3"]
+  }
 
   constructor() { }
 
